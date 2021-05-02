c003ff429906e85018cb09d951ee32ecb0323782
samarth kapoor <sam.kapoor.589.sk@gmail.com>
2021-05-02 23:40:38 +0530

    removed creation of rabbitmq queues

--- src/main/java/com/docker/sandbox/config/rabbitmq/MessagingConfig.java
+++ src/main/java/com/docker/sandbox/config/rabbitmq/MessagingConfig.java
@@ -1,40 +1,15 @@
 package com.docker.sandbox.config.rabbitmq;
 
-import com.docker.sandbox.testcase.TestCaseResponse;
-import lombok.Setter;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.ClassMapper;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "completed.testcases")
-@Setter
 public class MessagingConfig {
-
-    private String exchange;
-    private String queueName;
-    private String routingKey;
-
-    @Bean
-    public Queue queue() {
-        return new Queue(queueName);
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(exchange);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(routingKey);
-    }
 
     @Bean
     public MessageConverter converter() {
